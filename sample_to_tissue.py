890c2e24525d605db1cb7478f3dbc902f89bab07
mscwilson <mirandascwilson@gmail.com>
2015-04-10 16:50:26 +0100

    Using numpy

--- sample_to_tissue.py
+++ sample_to_tissue.py
@@ -1,13 +1,16 @@
+import numpy as np
+
+
 # Produces csv file with only results for my GOIs
 def extract_genes_of_interest():
 
     selected_genes_results = []
     genes_of_interest = ["ITPKA","ITPKB","ITPKC","IPMK", "ITPK1", "IPPK", "IP6K1", "IP6K2", "IP6K3", "PPIP5K1", "PPIP5K2"]
 
-    # this file is 1.6GB
+    # .gct is a kind of tab-separated txt file, this one is 1.6GB
     with open("GTEx_Analysis_2014-01-17_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct", "r") as rna_seq_data:
         for line in rna_seq_data:
-            temp = line.split("\t")
+            temp = line.strip().split("\t")
             if temp[0] == "Name": # this is the header line, contains sample information
                 selected_genes_results.append(temp)
             elif (len(temp)> 2) and (temp[1] in genes_of_interest): # these are results lines
@@ -22,41 +25,69 @@ def extract_genes_of_interest():
 
 # Takes tissue information from Annotations file, adds it to the csv txt from extract_genes_of_interest
 def add_tissue_type_to_columns():
-    pass
+    tissue_type_dict = {}
 
-from_samples_file = []
-sample_dict = {}
+    with open("GTEx_Data_2014-01-17_Annotations_SampleAttributesDS.txt", "r") as SampleAttributes_file:
+        for line in SampleAttributes_file:
+            temp = line.strip().split("\t")
+            if temp[0] != "SAMPID":
+                try:
+                    tissue_type_dict[temp[0]] = temp[5] + ": " + temp[6]
+                    # do a tuple or list here if want to separate the tissues/subtissues after all
+                except:
+                    print "temp to dict failed, %r" % temp
 
-with open("GTEx_Data_2014-01-17_Annotations_SampleAttributesDS.csv", "r") as SampleAttributes_file:
-    for line in SampleAttributes_file:
-        from_samples_file.append(line.split("\t")) #  each line is now a list, inside main list
+    #print tissue_type_dict.keys()
 
-from_samples_file.pop(0) # remove list item at index 0
+    with open("output_GOI_only.txt", "r") as results_file, open("output_GOI_with_tissue_types.txt", "w") as output:
+        sample_codes = results_file.readline().strip().split(",") # NB strip before split
+        actual_results = results_file.read()
+        #print actual_results
 
-for i in from_samples_file:
-    sample_dict[i[0]] = i[5:7] # make key from 1st index of each inner list, with values as list containing 2 items
+        tissues_in_order = []
 
+        #print sample_codes
+        for i in sample_codes:
+            if i in tissue_type_dict.keys():
+                #print "yes it's here %r" % i
+                tissues_in_order.append(tissue_type_dict[i])
+            else:
+                print "no: %r" % i
+                tissues_in_order.append("")
 
-# with open("output_GOI_only.txt", "w") as translated_results:
-#     for i in selected_genes_results:
-#         try:
-#             tissue_plus = sample_dict[i]
-#             output = i + "\t" + tissue_plus[0] + "\t" + tissue_plus[1] + "\n"
-#             print "output: %r" % output
-#             translated_results.write(output)
-#         except:
-#             #print "not in dict"
-#             pass
+        output.write(",".join(sample_codes) + "\n")
+        output.write(",".join(tissues_in_order) + "\n")
+        output.write(actual_results)
 
+# surely this doesn't really need to be a separate function. But how to get the list back out to upper scope?
+def get_names_list():
+    with open("output_GOI_with_tissue_types_test.csv", "r") as tt: # this test file is sorted by tissue too
+        tt.readline()
+        names_list = tt.readline().strip("\n").replace(" ","").replace("-", "").replace(":","").split(",") # Don't strip everything, those empty strings are important
+        return names_list
 
-# Could combine this function with the one below
-def aggregate_values_by_tissue_type():
-    pass
+# Tidies the results into an easy format for pasting into GraphPad Prism for pretty graphs
+# maybe it would be worth using Pandas so I can have row/column names?
 
-# Tidies the results into an easy format for pasting into GraphPad Prism
-# I doubt Python can make graphs as pretty as Prism's
 def get_mean_sd_n_for_gene_per_tissue_type():
-    pass
+
+    names_list = get_names_list()
+    #print names_list
+    test = np.genfromtxt("output_GOI_with_tissue_types_test.csv", delimiter=",", skip_header=2,  dtype=None, names=names_list)
+
+    print "test %r" % test
+
+    hippo_list = []
+    for i in test.dtype.names:
+        if "Hippo" in i:
+            hippo_list.append(i)
+    hippo_columns = test[hippo_list]
+    print "hippos %r" % hippo_columns
+    #print np.mean(hippo_columns)
 
 
 
+
+#extract_genes_of_interest()
+#add_tissue_type_to_columns()
+get_mean_sd_n_for_gene_per_tissue_type()
